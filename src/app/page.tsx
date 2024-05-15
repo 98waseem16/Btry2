e3048ba7eac2200de3776d541fd156dfa5583aed
98waseem16 <167998848+98waseem16@users.noreply.github.com>
2024-05-15 22:24:56 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default async function HomePage() {
     <>
       <SiteNavbar />
       <main className="flex flex-col items-center justify-center h-screen text-center space-y-2">
-        <h1 className="text-4xl font-medium">ChatBotKit Whitelabel</h1>
-        <p>Open-source white-label solution on top of ChatBotKit</p>
-        <p className="font-bold">Extend and Customize</p>
+        <h1 className="text-4xl font-medium">Breyta</h1>
+        <p>GPTs tailored to your business</p>
+        <p className="font-bold">Try it out now!</p>
       </main>
     </>
   )
