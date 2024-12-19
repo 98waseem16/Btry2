2ead346d469047f54752da9afd34ab01dccde4eb
98waseem16 <167998848+98waseem16@users.noreply.github.com>
2024-12-19 10:29:39 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default async function HomePage() {
     <>
       <SiteNavbar />
       <main className="flex flex-col items-center justify-center h-screen text-center space-y-2">
-        <h1 className="text-4xl font-medium">Breyta</h1>
-        <p>GPTs tailored to your business</p>
+        <h1 className="text-4xl font-medium">INQ-AI</h1>
+        <p>Create AI agents to handle guest queries, elevate experiences, and simplify communication</p>
         <p className="font-bold">Try it out now!</p>
       </main>
     </>
